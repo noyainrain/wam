2c47e8eb3703cc748fe53a823f80489d61dc8e3d
Sven <sven.jms@gmail.com>
2015-09-14 11:53:56 -0400

    Ignore postgresql setup error

--- util/setup-postgresql.sh
+++ util/setup-postgresql.sh
@@ -7,6 +7,7 @@ WAMUSER=$USER
 sudo apt-get -qy install postgresql python3-psycopg2
 # TODO: better way? superuser needed for extensions, but can drop all databases
 # etc.
-sudo -u postgres psql -c "CREATE USER $WAMUSER WITH SUPERUSER"
+# Ignore if the user already exists
+sudo -u postgres psql -c "CREATE USER $WAMUSER WITH SUPERUSER" || true
 #sudo -u postgres psql -c "CREATE USER $WAMUSER WITH CREATEDB CREATEROLE"
 #sudo -u postgres psql -c "ALTER USER $WAMUSER WITH SUPERUSER"
